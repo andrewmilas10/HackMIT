f34bf3348c741d2ea7c44aa39ebcdaeeb961c869
unknown <andrewmilas10@gmail.com>
2022-10-01 22:53:03 -0400

    Started socket code

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_socketio import SocketIO, emit
+from flask_socketio import join_room, leave_room
+
 app = Flask(__name__, static_folder='./frontend/build', static_url_path='/')
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -10,8 +14,24 @@ def not_found(e):
 
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
-    return "Hello World!"
+    return "Hello World!safdsaf AHIHIA"
+
+
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    # send(username + ' has entered the room.', to=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    # send(username + ' has left the room.', to=room)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+    socketio.run(app, host='0.0.0.0', port=105)
