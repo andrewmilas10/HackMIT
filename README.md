4516d0a19e13c3f320037ade4c78c7caffa6c540
Daniel Kim <dknj11902@gmail.com>
2022-10-01 20:17:55 -0400

    backend frontend setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HackMIT
+
+
+## Setup
+
+Start two integrated terminals in VS Code. In one of them, run `cd frontend && npm run start-backend` and in the other run `cd frontend && npm start`. Then navigate to `http://localhost:3000/` on the browser.
+
--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,4 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from "axios";
+
+function getData() {
+    axios({
+        method: "GET",
+        url: "/hello/",
+    })
+        .then((response) => {
+            const res = response.data;
+            alert(res);
+        }).catch((error) => {
+
+        })
+}
 
 export const Home = () => {
     return (
@@ -6,7 +20,7 @@ export const Home = () => {
             <section className="section">
                 <h1 className="title">Spotify Party!</h1>
 
-                <button class="button">Log into Spotify</button>
+                <button class="button" onClick={getData}>Log into Spotify</button>
 
                 <button class="button">Join room</button>
             </section>
