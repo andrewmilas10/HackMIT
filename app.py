07ba070810c6e3120d8f3a55b36301347ecb714b
unknown <andrewmilas10@gmail.com>
2022-10-01 20:32:44 -0400

    Initialized flask for heroku

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__, static_folder='./build', static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+
+@app.route('/hello/', methods=['GET', 'POST'])
+def welcome():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)
